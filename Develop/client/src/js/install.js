55aadfd81ab7af6c2b359ab46bd865e0b0207ad7
Jaxson20 <jaxsonfellhauerschool@gmail.com>
2023-10-25 10:51:50 -0500

    setting up js files

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -1,11 +1,34 @@
 const butInstall = document.getElementById('buttonInstall');
+let deferredPrompt;
 
-// Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+  event.preventDefault();
+  
+  deferredPrompt = event;
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+  butInstall.style.display = 'block';
+});
+
+butInstall.addEventListener('click', async () => {
+  if (deferredPrompt) {
+
+    deferredPrompt.prompt();
+
+    const choiceResult = await deferredPrompt.userChoice;
+
+    if (choiceResult.outcome === 'accepted') {
+      console.log('User accepted the PWA installation');
+    } else {
+      console.log('User declined the PWA installation');
+    }
+
+    deferredPrompt = null;
+
+    butInstall.style.display = 'none';
+  }
+});
+
+window.addEventListener('appinstalled', (event) => {
+  console.log('PWA was installed.', event);
+});
