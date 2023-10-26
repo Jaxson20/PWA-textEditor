ad7aa5fffe78209abd1ed6f52abd723f1a6d9e78
Jaxson20 <jaxsonfellhauerschool@gmail.com>
2023-10-26 11:29:46 -0500

    install and sw

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -27,4 +27,18 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  
+  ({ request }) => request.destination === 'script' || request.destination === 'style',
+  new StaleWhileRevalidate({
+    cacheName: 'assets-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxEntries: 50, 
+      }),
+    ],
+  })
+);
--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -1,36 +1,25 @@
 const butInstall = document.getElementById('buttonInstall');
-let deferredPrompt;
 
 window.addEventListener('beforeinstallprompt', (event) => {
-
+  window.deferredPrompt = event;
+  butInstall.classList.toggle('hidden', false);
   event.preventDefault();
-  
-  deferredPrompt = event;
-
-  butInstall.style.display = 'block';
 });
 
 butInstall.addEventListener('click', async () => {
-  if (deferredPrompt) {
+  
+  const promptEvent = window.deferredPrompt;
 
-    deferredPrompt.prompt();
-
-    const choiceResult = await deferredPrompt.userChoice;
-
-    if (choiceResult.outcome === 'accepted') {
-      console.log('User accepted the PWA installation');
-    } else {
-      console.log('User declined the PWA installation');
-    }
-
-    deferredPrompt = null;
-
-    butInstall.style.display = 'none';
+  if (!promptEvent) {
+    return;
   }
+
+  promptEvent.prompt();
+  window.deferredPrompt = null;
+  butInstall.classList.toggle('hidden', true);
 });
 
 window.addEventListener('appinstalled', (event) => {
-  console.log('PWA was installed.', event);
+  window.deferredPrompt = null;
 });
-
 //This is doen I believe
